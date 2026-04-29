3c5b5f799dfa263080739bf917bbf8fa7c7d0008
piyushpatel-137 <piyushpatel.121619@gmail.com>
2026-04-29 22:55:29 +0530

    bravo service

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -2,8 +2,9 @@ const SibApiV3Sdk = require("sib-api-v3-sdk");
 
 const sendOtpEmail = async ({ email, otp, purpose }) => {
   const client = SibApiV3Sdk.ApiClient.instance;
-  const apiKey = client.authentications["api-key"];
-  apiKey.apiKey = process.env.BREVO_API_KEY;
+
+  const apiKeyInstance = client.authentications["api-key"];
+  apiKeyInstance.apiKey = process.env.BREVO_API_KEY;
 
   const tranEmailApi = new SibApiV3Sdk.TransactionalEmailsApi();
 
